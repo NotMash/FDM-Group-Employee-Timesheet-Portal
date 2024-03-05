ede3a8beb96edf8ceaf652717e6b231aa54d35d4
delterr <hasanahmadcodes@gmail.com>
2024-03-05 22:48:59 +0000

    added basic model proof of concept

--- backend/timesheets/__init__.py
+++ backend/timesheets/__init__.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost/timesheets'
 db = SQLAlchemy(app)
+app.app_context().push()
 
 from timesheets import routes
--- backend/timesheets/routes.py
+++ backend/timesheets/routes.py
@@ -5,4 +5,8 @@ from timesheets.models import User
 
 @app.route("/")
 def home():
-    return "<h1>Hello, world!</h1>"
+    users = User.query.all()
+    x = ""
+    for i in users:
+        x += i.username
+    return render_template("info.html", users=users)
