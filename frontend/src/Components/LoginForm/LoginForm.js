c257b572734aa45e6539685c05943231172e8551
Luke Liew Mouawad <ec22711@qmul.ac.uk>
2024-03-13 21:03:43 +0000

    added user type to login page

--- frontend/src/Components/LoginForm/LoginForm.js
+++ frontend/src/Components/LoginForm/LoginForm.js
@@ -7,10 +7,12 @@ function LoginForm()
 {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [userType, setUserType] = useState('Consultant');
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        const loginDetails = {username, password}
+        const loginDetails = {username, password, userType}
+        console.log(loginDetails)
         fetch('https://localhost:5000/login', {
             method: "POST",
             headers: {"Content-Type": "application/json",
@@ -24,14 +26,27 @@ function LoginForm()
                 <ul>
                     <li>
                         <figure>
-                            <img src="./user_icon.png"></img>
+                            <img className={styles.UserIcon} src="./user_icon.png"></img>
                         </figure>
+                    </li>
+                    <li>
+                        <label>User Type</label>
+                    </li>
+                    <li>
+                        <select className={styles.selectInput}
+                        value={userType}
+                        onChange={(e) => setUserType(e.target.value)}>
+                            <option>Consultant</option>
+                            <option>Line Manager</option>
+                            <option>Finance Team Member</option>
+                        </select>
                     </li>
                     <li>
                         <label for="username">Username</label>
                     </li>
                     <li>
                         <input type="username" id="uname" name="u_name" required
+                        className={styles.input}
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}></input>
                     </li>
@@ -39,12 +54,13 @@ function LoginForm()
                         <label for="password">Password</label>
                     </li>
                     <li>
-                        <input type="password" id="pword" name="u_password" required                        
+                        <input type="password" id="pword" name="u_password" required   
+                        className={styles.input}                     
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}></input>
                     </li><br></br>
                     <li>
-                        <a href="">Forgot Password?</a>
+                        <a id={styles.forgotPass} href="">Forgot Password?</a>
                     </li><br></br>
                     <li>
                         <input id="submit_button" type="submit" name="submit_btn" value="Submit"></input>
