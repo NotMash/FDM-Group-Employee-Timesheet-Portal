640bbaa83a3ef8b50be7f84457553a573320f205
delterr <hasanahmadcodes@gmail.com>
2024-03-19 15:18:23 +0000

    i was on the wrong branch lmao

--- backend/timesheets/__init__.py
+++ backend/timesheets/__init__.py
@@ -6,11 +6,12 @@ from flask_session import Session
 from flask_migrate import Migrate
 
 
-
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///timesheets.db"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:1234@localhost/timesheets'
+
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///timesheets.db"
 
 
 app.config["SESSION_TYPE"] = "filesystem"  # Use the server's filesystem
