cf163eda64ce27ca9183819e43dfd177f8f06156
Luke Liew Mouawad <ec22711@qmul.ac.uk>
2024-04-02 21:21:01 +0100

    view timesheet page now displays data for days in the table

--- frontend/src/Components/View_Timesheet_Page/TimesheetTable.js
+++ frontend/src/Components/View_Timesheet_Page/TimesheetTable.js
@@ -1,79 +1,89 @@
+import ViewTimesheetHeader from "../../Components/View_Timesheet_Page/ViewTimesheetHeader";
 import styles from './TimesheetTable.module.css';
 import { useState, useEffect } from 'react';
 
 export default function TimesheetTable() {
 
     const [timesheets, setTimesheets] = useState([]);
+
+    var arrayOfDays = []
+    var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
       
     useEffect(() => {
-        fetch('http://127.0.0.1:5000/list_weekly_timesheets', {
-                method: "GET",
-                headers: { "Content-Type": "application/json" },
-                credentials: 'include',
-            }).then(response => {
-                if (response.ok) {
-                    return response.json();
-                } 
-                else {
-                    throw new Error('User Creation Failed with Status: ' + response.status);
-                }
-            }).then(data => {
-                // Data fetched successfully
-                console.log(data)
-                setTimesheets(data);
-            }).catch(error => {
-                console.error(error);
-            });
+        const fetchData = async () =>{
+            try{
+                await fetch('http://127.0.0.1:5000/list_weekly_timesheets', {
+                        method: "GET",
+                        headers: { "Content-Type": "application/json" },
+                        credentials: 'include',
+                    }).then(response => {
+                        if (response.ok) {
+                            return response.json();
+                        } 
+                        else {
+                            throw new Error('User Creation Failed with Status: ' + response.status);
+                        }
+                    }).then(data => {
+                        // Data fetched successfully
+                        console.log(data)
+                        setTimesheets(data);
+                    }).catch(error => {
+                        console.error(error);
+                    });
+            } catch(error) {
+                console.log("error fetching data")
+            }
+        };
+        fetchData();
     }, []);
 
+    if(timesheets === null) {
+        return(
+            <p>Loading data</p>
+        )
+    }
     console.log("stored stuff:",timesheets)
 
-    return(
+    let counter = 0
+    Object.entries(timesheets).map(entry => {
+        arrayOfDays.push(entry[1])
+        const [day, month, year] = arrayOfDays[counter].day.split('/');
+        const formattedDate = `${month}/${day}/${year}`; 
+        const currentDay = new Date(formattedDate).getDay()
+        arrayOfDays[counter].day_of_week = daysOfWeek[currentDay]
+        console.log(arrayOfDays[counter].day_of_week)
+        counter++
+    })
+
+return ( 
+    <>
+        {arrayOfDays.length > 0 && <ViewTimesheetHeader currentWeek={arrayOfDays[0].week_start}/>}
         <div>
             <table className={styles.timesheetTable}>
-                <tr className={styles.tableRow}>
-                    <th className={styles.tableData}>DAY</th>
-                    <th className={styles.tableData}>DATE</th>
-                    <th className={styles.tableData}>START TIME</th>
-                    <th className={styles.tableData}>END TIME</th>
-                    <th className={styles.tableData}>HOURS WORKED</th>
-                </tr>
-                <tr className={styles.tableRow}>
-                    <td className={styles.tableData}>Monday</td>
-                    <td className={styles.tableData}>01/04/2024</td>
-                    <td className={styles.tableData}>09:05</td>
-                    <td className={styles.tableData}>17:30</td>
-                    <td className={styles.tableData}>8 Hours <br/> 25 Minutes</td>
-                </tr>
-                <tr className={styles.tableRow}>
-                    <td className={styles.tableData}>Tuesday</td>
-                    <td className={styles.tableData}>02/04/2024</td>
-                    <td className={styles.tableData}>09:05</td>
-                    <td className={styles.tableData}>17:30</td>
-                    <td className={styles.tableData}>8 Hours <br/> 25 Minutes</td>
-                </tr>
-                <tr className={styles.tableRow}>
-                    <td className={styles.tableData}>Wednesday</td>
-                    <td className={styles.tableData}>03/04/2024</td>
-                    <td className={styles.tableData}>09:05</td>
-                    <td className={styles.tableData}>17:30</td>
-                    <td className={styles.tableData}>8 Hours <br/> 25 Minutes</td>
-                </tr>
-                <tr className={styles.tableRow}>
-                    <td className={styles.tableData}>Thursday</td>
-                    <td className={styles.tableData}>04/04/2024</td>
-                    <td className={styles.tableData}>09:05</td>
-                    <td className={styles.tableData}>17:30</td>
-                    <td className={styles.tableData}>8 Hours <br/> 25 Minutes</td>
-                </tr>
-                <tr className={styles.tableRow}>
-                    <td className={styles.tableData}>Friday</td>
-                    <td className={styles.tableData}>05/04/2024</td>
-                    <td className={styles.tableData}>09:05</td>
-                    <td className={styles.tableData}>17:30</td>
-                    <td className={styles.tableData}>8 Hours <br/> 25 Minutes</td>
-                </tr>
+                <thead>
+                    <tr className={styles.tableRow}>
+                        <th className={styles.tableData}>DAY</th>
+                        <th className={styles.tableData}>DATE</th>
+                        <th className={styles.tableData}>START TIME</th>
+                        <th className={styles.tableData}>END TIME</th>
+                        <th className={styles.tableData}>HOURS WORKED</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {arrayOfDays.map((dayData, index) => (
+                        <tr key={index} className={styles.tableRow}>
+                            <td className={styles.tableData}>{dayData.day_of_week}</td>
+                            <td className={styles.tableData}>{dayData.day}</td>
+                            <td className={styles.tableData}>{dayData.start_work}</td>
+                            <td className={styles.tableData}>{dayData.end_work}</td>
+                            <td className={styles.tableData}>{dayData.hours_worked}</td>
+                        </tr>
+                    ))}
+                </tbody>
             </table>
         </div>
-    )
+    </>
+);
+
 }
--- frontend/src/Pages/Consultant/view_current_timesheet_page.js
+++ frontend/src/Pages/Consultant/view_current_timesheet_page.js
@@ -1,4 +1,3 @@
-import ViewTimesheetHeader from "../../Components/View_Timesheet_Page/ViewTimesheetHeader";
 import ConsultantDetails from "../../Components/View_Timesheet_Page/ConsultantDetails";
 import DailyRate from "../../Components/View_Timesheet_Page/DailyRate";
 import TimesheetTable from "../../Components/View_Timesheet_Page/TimesheetTable";
@@ -9,11 +8,10 @@ function ViewCurrentTimesheetPage () {
     document.title = "Current Timesheet Viewer Page";
 
     return(<>
-        <ViewTimesheetHeader/>
         <ConsultantDetails/>
         <DailyRate/>
         <TimesheetTable/>
-        <Link to="/"><button className={styles.backButton}>Back</button></Link>
+        <Link to="/consultant_home_page"><button className={styles.backButton}>Back</button></Link>
     </>)
 }
 
