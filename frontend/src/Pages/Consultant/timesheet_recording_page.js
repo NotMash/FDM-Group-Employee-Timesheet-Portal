3ae8668284c5dae4789d49d3f3381e7a5cf3b3c4
krish <114755077+krish2903@users.noreply.github.com>
2024-04-02 22:31:27 +0000

    Finished create timesheet page

--- frontend/src/Pages/Consultant/timesheet_recording_page.js
+++ frontend/src/Pages/Consultant/timesheet_recording_page.js
@@ -1,21 +1,25 @@
-import TimesheetFormDay from "../../Components/Timesheet_Recording_Page/Form_At_Day/timesheet_form_day"
-import TimesheetPageLinks from "../../Components/Timesheet_Recording_Page/Timesheet_Links/timesheet_page_links"
+import TimesheetFormDay from "../../Components/Timesheet_Recording_Page/Form_At_Day/timesheet_form_day";
+import TimesheetPageLinks from "../../Components/Timesheet_Recording_Page/Timesheet_Links/timesheet_page_links";
+import styles from "./timesheet_recording_page.module.css";
 
-function TimesheetRecordingPage(){
-    return(
+function TimesheetRecordingPage() {
+    return (
         <>
-            <h1>Timesheet Recording Page</h1><br/>
+            <h1 className={styles.heading}>Timesheet Recording Page</h1>
 
             {/*this just adds days to the timesheet form*/}
             {/*<TimesheetFormDay day="Monday"/>*/}
             {/*<TimesheetFormDay day="Tuesday"/>*/}
             {/*<TimesheetFormDay day="Wednesday"/>*/}
             {/*<TimesheetFormDay day="Thursday"/>*/}
-
-            <TimesheetFormDay day="Friday"/><br/>
-            <TimesheetPageLinks/>
+            <div className={styles.mainContainer}>
+                <div className={styles.weekContainer}>
+                    <TimesheetFormDay day="Monday" />
+                </div>
+                <TimesheetPageLinks />
+            </div>
         </>
-    )    
+    )
 }
 
 export default TimesheetRecordingPage
