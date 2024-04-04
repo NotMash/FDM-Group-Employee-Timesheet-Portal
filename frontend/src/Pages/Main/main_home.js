7ac34bed783172ca80990ae4bb7f0d27bff2aef4
Araza25 <120424159+Araza25@users.noreply.github.com>
2024-04-04 14:35:56 +0100

    Update landing page background and logo

--- frontend/src/Pages/Main/main_home.js
+++ frontend/src/Pages/Main/main_home.js
@@ -3,7 +3,7 @@ import styles from "./MainHome.module.css";
 
 import React from 'react';
 
-import logo from './Images/FDMLogo.jpeg';
+import logo from './Images/footer-logo.svg';
 
 
 //get the current time and output greeting based on time
@@ -25,18 +25,15 @@ function getTimeGreeting(){
 function MainHome(){
     document.title = "Landing Page";
     return (
-       <>
-          <div className={styles.landingPageBackground}> {/* Background container */}
-            <div className={styles.logoBar}> {/* Black bar container */}
-              <img src={logo} alt="FDM Logo" className={styles.logo}/>
-            </div>
-            <div id={styles.landing_page} className={styles.MainHome}>
-              <h1>FDM Timesheets App</h1>
-              <h2>Good {getTimeGreeting()}</h2>
-              <Link className={styles.Link} to="/login_page"><p>Login</p></Link>
-            </div>
+        <div className={styles.landingPageBackground}> {/* Background container */}
+          <div className={styles.logoContainer}>
+            <img src={logo} alt="FDM Logo" className={styles.logo}/>
           </div>
-        </>
+          <div id={styles.landing_page} className={styles.MainHome}>
+            <h2>Good {getTimeGreeting()}</h2>
+            <Link className={styles.Link} to="/login_page"><p>Login</p></Link>
+          </div>
+        </div>
     )
 }
 
