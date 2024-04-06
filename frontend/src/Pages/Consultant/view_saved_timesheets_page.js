f8fc3c05f6f70a7c3b8d7fc7b44226934ba04f8c
Luke Liew Mouawad <ec22711@qmul.ac.uk>
2024-04-06 14:41:00 +0100

    added filtering options to consultant view saved timesheets page

--- frontend/src/Pages/Consultant/view_saved_timesheets_page.js
+++ frontend/src/Pages/Consultant/view_saved_timesheets_page.js
@@ -11,7 +11,55 @@ export default function ViewSavedTimesheetsPage() {
     {pageName : "View Saved Timesheets", pageLink : "/view_saved_timesheet", iconPath : "./Home_Page_Icons/Consultant/view_saved_timesheets.svg"}]
     
     const [timesheets, setTimesheets] = useState([]);
+    const [selectedTimeFilter, setSelectedTimeFilter] = useState('')
+    const [selectedStatusFilter, setSelectedStatusFilter] = useState('')
+    const [filteredTimesheets, setFilteredTimesheets] = useState([])
     var timesheetsArray = []
+
+    //set default options for filters
+    if(selectedTimeFilter == '' && selectedStatusFilter == '') {
+        setSelectedTimeFilter('new_to_old')
+        setSelectedStatusFilter('all')
+        console.log(selectedTimeFilter)
+    }
+
+
+
+    //handle when user selects different filters
+    const handleTimeFilterChange = (event) => {
+        setSelectedTimeFilter(event.target.value)
+    } 
+    useEffect(() => {
+        console.log(selectedTimeFilter)
+        if(timesheetsArray.length != 0) {
+            //filter results by time
+            const filteredTimeArray = selectedTimeFilter == 'new_to_old' ? timesheetsArray.reverse() : timesheetsArray
+            setFilteredTimesheets(filteredTimeArray)
+
+            //filter results by status
+            if(selectedStatusFilter != 'all') {
+                setFilteredTimesheets(filteredTimeArray.filter(item => item.status === selectedStatusFilter))
+            }
+        }
+    }, [selectedTimeFilter]);
+
+
+    const handleStatusFilterChange = (event) => {
+        setSelectedStatusFilter(event.target.value)
+    }
+    useEffect(() => {
+        console.log(selectedStatusFilter)
+        if(timesheetsArray.length != 0) {
+            const filteredTimeArray = selectedTimeFilter == 'new_to_old' ? timesheetsArray.reverse() : timesheetsArray
+            setFilteredTimesheets(filteredTimeArray)
+
+            //filter results by status
+            if(selectedStatusFilter != 'all') {
+                setFilteredTimesheets(filteredTimeArray.filter(item => item.status === selectedStatusFilter))
+            }
+            console.log(filteredTimesheets, "filtered timesheets")
+        }
+    }, [selectedStatusFilter]); 
 
     //api call
     useEffect(() => {
@@ -61,7 +109,24 @@ export default function ViewSavedTimesheetsPage() {
             <Navbar homePageTitle="Consultant Home Page" homePageLink="/consultant_home_page" links={links}/>
             <main className={styles.main}>
                 <h1>Saved Timesheets</h1>
-                {timesheetsArray.map(timesheetData => (
+
+                <p>{selectedTimeFilter} : {selectedStatusFilter}</p>
+
+                <label className={styles.filterTitle}>Filter Timesheets by Time</label>
+                <select value={selectedTimeFilter} onChange={handleTimeFilterChange}>
+                    <option value="new_to_old">Newest to Oldest</option>
+                    <option value="old_to_new">Oldest to Newest</option>
+                </select>
+
+                <label className={styles.filterTitle}>Filter Timesheets by Status</label>
+                <select value={selectedStatusFilter} onChange={handleStatusFilterChange}>
+                    <option value="all">All</option>
+                    <option value="approved">Approved</option>
+                    <option value="disapproved">Disapproved</option>
+                    <option value="pending">Pending</option>
+                </select>
+
+                {filteredTimesheets.map(timesheetData => (
                     <TimesheetCard day={timesheetData.day}
                     workStart={timesheetData.start_work} 
                     workEnd={timesheetData.end_work}
