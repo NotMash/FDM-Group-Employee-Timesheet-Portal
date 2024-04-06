15821c90cea8b19bce5f99aa3bfb71f07eb22817
Ilyas Mohamed Ali <i.ali@se22.qmul.ac.uk>
2024-04-06 19:31:50 +0100

    finance team page changes

--- frontend/src/Pages/Finance_Team/finance_team_mem_home_page.js
+++ frontend/src/Pages/Finance_Team/finance_team_mem_home_page.js
@@ -4,14 +4,28 @@ import styles from "./finance_team_member_home_page.module.css"; // Import the C
 import Navbar from "../../Components/Global/Navbar";
 
 function FinanceTeamMemberHomePage() {
-  document.title = "Finance Team Home Page"
+  document.title = "Finance Team Home Page";
 
-  let links = [{pageName : "Set Hourly Rate Page", pageLink : "/set_hourly_rate", iconPath : "./Home_Page_Icons/Consultant/record_timesheet.svg"},
-              {pageName : "View Timesheet Page", pageLink : "/view_timesheet", iconPath : "./Home_Page_Icons/Consultant/view_timesheet.svg"}]
+  let links = [
+    {
+      pageName: "Set Hourly Rate Page",
+      pageLink: "/set_hourly_rate",
+      iconPath: "./Home_Page_Icons/Consultant/record_timesheet.svg",
+    },
+    {
+      pageName: "View Timesheet Page",
+      pageLink: "/view_timesheet",
+      iconPath: "./Home_Page_Icons/Consultant/view_timesheet.svg",
+    },
+  ];
 
   return (
     <>
-      <Navbar homePageTitle="Finance Team Home Page" homePageLink="/finance_team_member_home_page" links={links}/>
+      <Navbar
+        homePageTitle="Finance Team Home Page"
+        homePageLink="/finance_team_member_home_page"
+        links={links}
+      />
       <div className={styles.financeTeamMemberContainer}>
         <h1 className={styles.financeTeamMemberHeading}>
           Finance Team Member Home Page
